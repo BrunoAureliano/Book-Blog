1b935d16b0f0fa849b8ba3afb9a483705c5928de
Bruno Henrique Aureliano <brunoaureliano2006@gmail.com>
2025-05-12 15:49:07 -0300

    :sparkles: feat: adicionar configurações

--- app.js
+++ app.js
@@ -7,10 +7,39 @@ import { fileURLToPath } from "url"
 import mongoose from "mongoose"
 import passport from "passport"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
 
 const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+dotenv.config()
+
+// Configurações
+
+    // Body-parser
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+
+    // Handlebars
+    const handlebars = ExpressHandlebars.create({
+        defaultLayout: 'main',
+        runtimeOptions: {
+            allowProtoMethodsByDefault: true,
+            allowProtoPropertiesByDefault: true
+        }
+    })
+    app.engine('handlebars', handlebars.engine)
+    app.set('view engine', 'handlebars')
+
+    // Mongoose
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Mongo Conectado!')
+    } catch (err) {
+        console.log(`Erro ao conectar com o Mongo! ${err}`)
+    }
+
 
 app.get('/', (req, res) => {
     res.send('homepage')
