2b1b53632172098c72e95d0c18f5f43be3bb5cff
Bruno Henrique Aureliano <brunoaureliano2006@gmail.com>
2025-05-16 13:45:28 -0300

    :recycle: refactor: alterar diretório das rotas de cadastro e login

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ import bodyParser from "body-parser"
 import user from './routes/user.js'
 import admin from './routes/admin.js'
 
-import './models/Usuarios.js'
-const Usuario = mongoose.model('usuarios')
 
 const app = express()
 
@@ -69,41 +67,8 @@ dotenv.config()
 app.use('/user', user)
 app.use('/admin', admin)
 
-// Página de Cadastro
 app.get('/', (req, res) => {
-    res.render('cadastro')
-})
-
-app.post('/', async (req, res) => {
-    try {
-        const usuario = await Usuario.findOne({ email: req.body.email })
-        if (usuario) {
-            req.flash('error_msg', 'Já existe uma conta com este email!')
-            res.redirect('/')
-        } else {
-            const newUser = {
-                nome: req.body.nome,
-                email: req.body.email,
-                senha: req.body.senha
-            }
-
-            await new Usuario(newUser).save()
-            req.flash('success_msg', 'Usuário cadastrado com sucesso')
-            res.redirect('/user')
-        }
-    } catch (err) {
-        req.flash('error_msg', 'Ocorreu um erro ao cadastrar o usuário. Tente novamente!')
-        res.redirect('/')
-    }
-})
-
-// Página de Login
-app.get('/login', async (req, res) => {
-    try {
-        res.render('login')
-    } catch (err) {
-
-    }
+    res.render('index')
 })
 
 app.listen(8080, () => {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,48 @@
 import express from 'express'
 const router = express.Router()
 
+import mongoose from 'mongoose'
+import '../models/Usuarios.js'
+const Usuario = mongoose.model('usuarios')
+
+
+// Página de Cadastro
+router.get('/cadastro', (req, res) => {
+    res.render('user/cadastro')
+})
+
+router.post('/cadastro', async (req, res) => {
+    try {
+        const usuario = await Usuario.findOne({ email: req.body.email })
+        if (usuario) {
+            req.flash('error_msg', 'Já existe uma conta com este email!')
+            res.redirect('/')
+        } else {
+            const newUser = {
+                nome: req.body.nome,
+                email: req.body.email,
+                senha: req.body.senha
+            }
+
+            await new Usuario(newUser).save()
+            req.flash('success_msg', 'Usuário cadastrado com sucesso')
+            res.redirect('/user')
+        }
+    } catch (err) {
+        req.flash('error_msg', 'Ocorreu um erro ao cadastrar o usuário. Tente novamente!')
+        res.redirect('/')
+    }
+})
+
+// Página de Login
+router.get('/login', async (req, res) => {
+    try {
+        res.render('user/login')
+    } catch (err) {
+
+    }
+})
+
 // Painel inicial do Usuário
     router.get('/', async (req, res) => {
         try {
